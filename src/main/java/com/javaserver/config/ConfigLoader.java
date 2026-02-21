6732a7df023260fbb4a66986f93bb8a8d4fc1457
maynaou <mohssinaynaou874@gmail.com>
2026-02-21 13:27:30 +0000

    premier setap add config.json

---
+++ src/main/java/com/javaserver/config/ConfigLoader.java
@@ -0,0 +1,23 @@
+package main.java.com.javaserver.config;
+
+// import java.nio.file.Files;
+// import java.nio.file.Paths;
+
+public class ConfigLoader {
+
+    // public static String readFile(String path) {
+    //     try {
+    //         byte[] bytes = Files.readAllBytes(Paths.get(path));
+    //         return new String(bytes);
+    //     } catch (Exception e) {
+    //         System.err.println("❌ Impossible de lire le fichier : " + e.getMessage());
+    //         System.exit(1);
+    //         return null;
+    //     }
+    // }
+
+    // public static void main(String[] args) {
+    //     String content = readFile("config.json");
+    //     System.out.println(content);
+    // }
+}
